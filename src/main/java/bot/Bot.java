5eec21c54ac6ec2f0a0774ef7eb69c9954b972cd
Vladimir Solovyov <svn131131@gmail.com>
2023-09-03 01:30:39 +0500

    222RabotaetSoobshenyaShlet-OstalosUbratOtladochnuyuInfu

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -8,10 +8,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.api.objects.User;
 
-import java.io.FileWriter;
+import java.io.*;
+import java.time.LocalDateTime;
 import java.util.*;
-
-import java.io.IOException;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -19,19 +18,21 @@ public class Bot extends TelegramLongPollingBot {
 
     Map<Long,String> baseUsers = new HashMap<>();
 
+    public Bot() {
+        ReadOnWrite.loadBaseUsers("C:/userBasesMapa.txt", baseUsers);
+    }
 
 
-    /*
-    Создать и получить токен бота нужно в @BotFather в Telegram.
-     */
+
+
     @Override
     public String getBotUsername() {
-        return "mybottJavamybot_bot"; // Метод, который возвращает username бота.
+        return "SportsBot10_bot"; // Метод, который возвращает username бота.
     }
 
     @Override
     public String getBotToken() {
-        return "6650591421:AAHrT3OMJqM5cgxOxZ3nQ9-GKQUXPuPAMtY"; // Метод, который возвращает token бота.
+        return "6493956819:AAHbHe4wsGPvvD97SfEyLJK5k2jJJZgIzZ0"; // Метод, который возвращает token бота.
     }
 
 
@@ -69,6 +70,10 @@ public class Bot extends TelegramLongPollingBot {
                     value += " " + lastName;
                 }
 
+                String currentDate = LocalDateTime.now().toString();
+                value += " " + currentDate;
+
+
                 System.out.println("Dobavka v mapu");
                 baseUsers.put(chatId, value); // Добавляем в хеш-карту
 
@@ -86,13 +91,23 @@ public class Bot extends TelegramLongPollingBot {
                 // Пример вывода значения из хеш-карты:
                 System.out.println("User Info: " + baseUsers.get(chatId));
 
-                try {
-                    FileWriter writer = new FileWriter("C:/userBasesMapa.txt", true);
-                    writer.write(chatId + " " + value + "\n");
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+                // Открытие файлового потока для чтения
+
+///////////////////////////////////////////////////////////////////////////////
+                if (!ReadOnWrite.checkChatIdExists("C:/userBasesMapa.txt", chatId)) {
+                    try (FileWriter writer = new FileWriter("C:/userBasesMapa.txt", true)) {
+                        writer.write(chatId + " " + value + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        // Обработка ошибки записи файла
+                    }
                 }
+
+
+                /////////////////////////////////////////////////////////////////////////////////////
+
+
                 System.out.println("333333333");
                 sendOk(chatId);
 
@@ -110,7 +125,7 @@ public class Bot extends TelegramLongPollingBot {
             } else if (text.equals("Vhodi")) {
                 System.out.println("Sdelalsy true");
                 dobavitNovogoUsera = true;
-            }else if(text.equals("S")){
+            }else if(text.equals("S")){ // @todo Метод для отладки
                 sendArrayDataToAll(new String[]{"data1", "data2", "data3"});
             }
 
@@ -169,21 +184,6 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-//    public void sendArrayDataToAll(String[] dataArray) {
-//
-//        System.out.println("Delaetsya sendArrayDataToAll");
-//
-//        List<Long> allowedChatIds = getAllowedChatIds(); // Получаем список разрешенных chatId
-//
-//        for (Long chatId : allowedChatIds) {
-//
-//            System.out.println("Sending data to chatId: " + chatId);
-//            sendArrayData(chatId, dataArray); // Отправляем массив строк каждому пользователю по chatId
-//        }
-//    }
-
-
-
 
     // Метод для получения списка всех разрешенных chatId
     private List<Long> getAllowedChatIds() {
@@ -216,170 +216,13 @@ public class Bot extends TelegramLongPollingBot {
 
 }
 
-// Метод для проверки, разрешен ли пользователь получать массив данных
-//    private boolean isUserAllowed(Long chatId) {
-//        // Здесь вы можете сравнить chatId с существующим массивом разрешенных юзернеймов
-//        // Для простоты я использовал хардкод с одним разрешенным юзернеймом: @GOLDGAME77777
-//        String allowedUsername = "@GOLDGAME77777";
-//        String chatUsername = "@" + getChatUsername(chatId);
-//
-//        return chatUsername.equals(allowedUsername);
-//    }
-
-
-// Метод для отправки массива строк пользователю по юзернейму
-//    private void sendArrayData(String username, List<String> dataArray) throws IOException {
-//        SendMessage sendMessage = new SendMessage();
-//        sendMessage.setText(String.join("\n", dataArray));
-//        sendMessage.setChatId(poluchenieChatIdPOUserName( username)); // Используем "@" для указания юзернейма
-//        try {
-//            execute(sendMessage); // Синхронное выполнение для простоты
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    public static Long poluchenieChatIdPOUserName(String username) throws IOException {
-//    String botToken = "6650591421:AAHrT3OMJqM5cgxOxZ3nQ9-GKQUXPuPAMtY" ;
-////    String username = "@GOLDGAME77777";
-//
-//    // Формирование URL запроса
-//    String urlString = "https://api.telegram.org/bot" + botToken + "/getChat?chat_id=" + username;
-//
-//    // Отправка GET-запроса и получение ответа
-//    URL url = new URL(urlString);
-//    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//        connection.setRequestMethod("GET");
-//
-//    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//    StringBuilder response = new StringBuilder();
-//    String line;
-//        while ((line = reader.readLine()) != null) {
-//        response.append(line);
-//    }
-//        reader.close();
-//
-//    // Извлечение chat ID из ответа
-//    String jsonResponse = response.toString();
-//    int chatIdStartIndex = jsonResponse.indexOf("\"id\":") + 5;
-//    int chatIdEndIndex = jsonResponse.indexOf(",", chatIdStartIndex);
-//    String chatIdString = jsonResponse.substring(chatIdStartIndex, chatIdEndIndex);
-//
-//    // Вывод chat ID в консоль
-//    long chatId = Long.parseLong(chatIdString);
-//        System.out.println("Chat ID для пользователя " + username + ": " + chatId);
-//
-//        return chatId;
-//}
-
-
-
 
 //    @Override
-//    public void onUpdateReceived(Update update) {
-//        if (update.hasMessage()) {
-//            String messageText = update.getMessage().getText(); // Получить текст сообщения
-//            Long chatId = update.getMessage().getChatId(); // Получить chatId
-//
-//            switch (messageText) {
-//                case "/start":
-//                    sendMessage(chatId, "Привет! Я помогу узнать погоду, просто напиши название города.");
-//                    break;
-//                case "/help":
-//                    sendMessage(chatId, "Чтобы узнать погоду, отправь название города. Если такого города нет, я промолчу!");
-//                    break;
-//                default:
-//                    if (isUserAllowed(chatId)) {
-//                        // Если пользователь разрешен, отправить ему массив строк
-//                        String[] dataArray = {"data1", "data2", "data3"}; // Пример массива строк
-//                        sendArrayData(chatId, dataArray);
-//                    }
-//            }
-//        }
+//    public String getBotUsername() {
+//        return "mybottJavamybot_bot"; // Метод, который возвращает username бота.
 //    }
-
-//    public void onUpdateReceived(Update update) {
-//        if (update.hasMessage()) {
-//            Message message = update.getMessage();
-//            Long chatId = message.getChatId(); // Получаем chat ID пользователя
-//            // Дальше можно использовать chatId для отправки сообщения обратно пользователю или для других действий.
 //
-//            // Пример вывода chat ID в консоль:
-//            System.out.println("Chat ID: " + chatId);
-//        }
-//    } //@todo получить чат ID  по сообщению от пользователя
-
-// Метод, отвечающий за обработку полученного сообщения.
-
 //    @Override
-//    public void onUpdateReceived(Update update) {
-//        if (update.hasMessage()) {
-//            Long chatId = update.getMessage().getChatId(); // Получить chatId
-//
-//            if (isUserAllowed(chatId)) {
-//                String[] dataArray = {"data1", "data2", "data3"}; // Пример массива строк
-//                sendArrayData(chatId, dataArray);
-//            }
-//        }
-//    }
-
-
-// Метод для отправки массива строк пользователю
-//    private void sendArrayData(Long chatId, List<String> dataArray) {
-//        SendMessage sendMessage = new SendMessage();
-//        sendMessage.setText(String.join("\n", dataArray));
-//        sendMessage.setChatId(chatId);
-//        try {
-//            execute(sendMessage); // Синхронное выполнение для простоты
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-// Метод для отправки массива строк всем разрешенным пользователям
-//    public void sendArrayDataToAll(List<String> dataArray) {
-//        List<Long> allowedChatIds = getAllowedChatIds(); // Получаем список разрешенных chatId
-//
-//        for (Long chatId : allowedChatIds) {
-//            sendArrayData(chatId, dataArray); // Отправляем массив строк каждому пользователю
-//        }
-//    }
-
-//    private String getChatUsername(Long chatId) {
-//        GetChatMember getChatMember = new GetChatMember();
-//        getChatMember.setChatId(chatId.toString());
-//        getChatMember.setUserId((long) chatId.intValue()); // Устанавливаем chatId в качестве userId
-//
-//        try {
-//            ChatMember chatMember = execute(getChatMember);
-//            User user = chatMember.getUser();
-//            if (user != null) {
-//                return user.getUserName(); // Возвращаем юзернейм пользователя
-//            }
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return ""; // Если имя пользователя не найдено, возвращаем пустую строку
-//    }
-
-
-
-// Точка входа (метод main, в котором происходит создание и инициализация бота
-//    public static void main(String[] args) throws IOException {
-//        try {
-//            Bot bot = new Bot();
-//            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-//            telegramBotsApi.registerBot(bot);
-//        } catch (TelegramApiException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        Bot bot = new Bot();
-//        bot.sendArrayDataToAll(new String[]{"data1", "data2", "data3"});
-//
-//
-//        System.out.println("Логика бота стартанула");
-//
-//
+//    public String getBotToken() {
+//        return "6650591421:AAHrT3OMJqM5cgxOxZ3nQ9-GKQUXPuPAMtY"; // Метод, который возвращает token бота.
 //    }
---
+++ src/main/java/bot/ReadOnWrite.java
@@ -0,0 +1,55 @@
+package bot;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+public class ReadOnWrite {
+
+
+    // Проверка есть ли такой юзер с таким чат ид или нет в файле
+    public static boolean checkChatIdExists(String filePath, Long chatId) {
+        try (FileReader reader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+
+            boolean chatIdExists = false;
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.startsWith(chatId.toString())) {
+                    chatIdExists = true;
+                    break;
+                }
+            }
+
+            return chatIdExists;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false; // или выбросить исключение для обработки выше
+        }
+    }
+
+    // Запись в мапу при инициализации из файла
+    public static void loadBaseUsers(String filePath, Map<Long, String> baseUsers) {
+        try (FileReader reader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(" ", 2);
+                Long chatId = Long.parseLong(parts[0].trim());
+                String value = parts[1].trim();
+                baseUsers.put(chatId, value);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Обработка ошибки чтения файла
+        }
+    }
+
+}
+
+
