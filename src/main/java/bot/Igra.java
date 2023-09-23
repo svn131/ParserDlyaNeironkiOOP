10c01939d9a59eb3ebbc69f7ea61e433f4830442
Vladimir Solovyov <svn131131@gmail.com>
2023-09-23 16:57:38 +0500

    2

--- src/main/java/bot/Igra.java
+++ src/main/java/bot/Igra.java
@@ -1,84 +1,46 @@
 package bot;
 
-import java.util.Objects;
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Igra {
 
     String o1;
     String o2;
 
+    private long startanula = System.currentTimeMillis();
+
+    long vremyaPoslednegoVzaimodestviya = System.currentTimeMillis();
+
+    boolean zamokResult = true;
+    boolean zamokWrite = false;
+    boolean zamokSamounichtoghitel = false;
+
+    long samounochtogitel = 2000000000000L;
+
+
     int seriinik;
 
     int time;
 
-    int totalOne1;
-    int totalOne2;
-    int totalOne3;
-    int totalOne4;
-    int totalOne5;
-    int totalOne6;
-    int totalOne7;
-    int totalOne8;
-    int totalOne9;
 
 
-    int totalTwo1;
-    int totalTwo2;
-    int totalTwo3;
-    int totalTwo4;
-    int totalTwo5;
-    int totalTwo6;
-    int totalTwo7;
-    int totalTwo8;
-    int totalTwo9;
+    Minute masivMinut[];
 
     int resultTotal;
-
-//    long startanula = System.currentTimeMillis();
-
-    long vremyaPoslednegoVzaimodestviya = System.currentTimeMillis();
-
-    ///////
-
-
-    double predlagaemyiTotalNa1Min;
-    double predlagaemyiTotalNa2Min;
-    double predlagaemyiTotalNa3Min;
-    double predlagaemyiTotalNa4Min;
-    double predlagaemyiTotalNa5Min;
-    double predlagaemyiTotalNa6Min;
-    double predlagaemyiTotalNa7Min;
-    double predlagaemyiTotalNa8Min;
-    double predlagaemyiTotalNa9Min;
-
-
-
-
-    boolean zamok1 = true;
-    boolean zamok2 = true;
-    boolean zamok3 = true;
-    boolean zamok4 = true;
-    boolean zamok5 = true;
-    boolean zamok6 = true;
-    boolean zamok7 = true;
-    boolean zamok8 = true;
-    boolean zamok9 = true;
-
-boolean zamokResult = true;
-boolean zamokWrite = false;
-//boolean zamokSamounichtoghitel = false;
-
-long samounochtogitel = 2000000000000L;
-
-
-
 
 
     public Igra(int seriinik) {
         this.seriinik = seriinik;
+        masivMinut = new Minute[]{new Minute(), new Minute(), new Minute(), new Minute(), new Minute(), new Minute(), new Minute(), new Minute(), new Minute()};
     }
 
-
+    public String getStartanula() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(startanula);
+        return "Стартанула " + sdf.format(date);
+    }
 
 
     @Override
@@ -100,60 +62,163 @@ long samounochtogitel = 2000000000000L;
         return Objects.hash(seriinik);
     }
 
-//    @Override
-//    public String toString() {
-//        return "Igra{" +
-//                "seriinik=" + seriinik +
-//                ", time=" + time +
-//                ", vremyaPoslednegoVzaimodestviya=" + vremyaPoslednegoVzaimodestviya + /// todo Убрать ?
-//                '}';
-//    }
-
-
     @Override
     public String toString() {
         return "Igra{" +
-                "seriinik=" + seriinik +
-                ", time=" + time +
-                ", totalOne1=" + totalOne1 +
-                ", totalOne2=" + totalOne2 +
-                ", totalOne3=" + totalOne3 +
-                ", totalOne4=" + totalOne4 +
-                ", totalOne5=" + totalOne5 +
-                ", totalOne6=" + totalOne6 +
-                ", totalOne7=" + totalOne7 +
-                ", totalOne8=" + totalOne8 +
-                ", totalOne9=" + totalOne9 +
-                ", totalTwo1=" + totalTwo1 +
-                ", totalTwo2=" + totalTwo2 +
-                ", totalTwo3=" + totalTwo3 +
-                ", totalTwo4=" + totalTwo4 +
-                ", totalTwo5=" + totalTwo5 +
-                ", totalTwo6=" + totalTwo6 +
-                ", totalTwo7=" + totalTwo7 +
-                ", totalTwo8=" + totalTwo8 +
-                ", totalTwo9=" + totalTwo9 +
-                ", resultTotal=" + resultTotal +
+                "o1='" + o1 + '\'' +
+                ", o2='" + o2 + '\'' +
                 ", vremyaPoslednegoVzaimodestviya=" + vremyaPoslednegoVzaimodestviya +
-                ", predlagaemyiTotalNa1Min=" + predlagaemyiTotalNa1Min +
-                ", predlagaemyiTotalNa2Min=" + predlagaemyiTotalNa2Min +
-                ", predlagaemyiTotalNa3Min=" + predlagaemyiTotalNa3Min +
-                ", predlagaemyiTotalNa4Min=" + predlagaemyiTotalNa4Min +
-                ", predlagaemyiTotalNa5Min=" + predlagaemyiTotalNa5Min +
-                ", predlagaemyiTotalNa6Min=" + predlagaemyiTotalNa6Min +
-                ", predlagaemyiTotalNa7Min=" + predlagaemyiTotalNa7Min +
-                ", predlagaemyiTotalNa8Min=" + predlagaemyiTotalNa8Min +
-                ", predlagaemyiTotalNa9Min=" + predlagaemyiTotalNa9Min +
-                ", zamok1=" + zamok1 +
-                ", zamok2=" + zamok2 +
-                ", zamok3=" + zamok3 +
-                ", zamok4=" + zamok4 +
-                ", zamok5=" + zamok5 +
-                ", zamok6=" + zamok6 +
-                ", zamok7=" + zamok7 +
-                ", zamok8=" + zamok8 +
-                ", zamok9=" + zamok9 +
                 ", zamokResult=" + zamokResult +
+                ", zamokWrite=" + zamokWrite +
+                ", zamokSamounichtoghitel=" + zamokSamounichtoghitel +
+                ", samounochtogitel=" + samounochtogitel +
+                ", seriinik=" + seriinik +
+                ", time=" + time +
+                ", resultTotal=" + resultTotal +
+                getStartanula() +
+                "лист минут" + Arrays.toString(masivMinut) +
                 '}';
     }
+
+
+    public void setPredlagaemyiBukmekermTotal(int timess, double predlagaemyiBukmekermTotal) {
+        switch (time / 60) {
+            case 1:
+                masivMinut[0].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+
+            case 2:
+                masivMinut[1].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+
+            case 3:
+                masivMinut[2].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+            case 4:
+                masivMinut[3].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+
+            case 5:
+                masivMinut[4].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+
+            case 6:
+                masivMinut[5].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+            case 7:
+                masivMinut[6].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+
+            case 8:
+                masivMinut[7].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+
+            case 9:
+                masivMinut[8].setPredlagaemyiBukmekermTotal(predlagaemyiBukmekermTotal);
+                break;
+        }
+
+
+
+
+    }
+    public void setTotalOneandTwo(int timess,  int totalOne, int totalTwo){
+
+
+        switch (time / 60) {
+            case 1:
+                masivMinut[0].setTotalOne(totalOne);
+                masivMinut[0].setTotalTwo(totalTwo);
+                break;
+            case 2:
+                masivMinut[1].setTotalOne(totalOne);
+                masivMinut[1].setTotalTwo(totalTwo);
+                break;
+            case 3:
+                masivMinut[2].setTotalOne(totalOne);
+                masivMinut[2].setTotalTwo(totalTwo);
+                break;
+            case 4:
+                masivMinut[3].setTotalOne(totalOne);
+                masivMinut[3].setTotalTwo(totalTwo);
+                break;
+            case 5:
+                masivMinut[4].setTotalOne(totalOne);
+                masivMinut[4].setTotalTwo(totalTwo);
+                break;
+            case 6:
+                masivMinut[5].setTotalOne(totalOne);
+                masivMinut[5].setTotalTwo(totalTwo);
+                break;
+            case 7:
+                masivMinut[6].setTotalOne(totalOne);
+                masivMinut[6].setTotalTwo(totalTwo);
+                break;
+            case 8:
+                masivMinut[7].setTotalOne(totalOne);
+                masivMinut[7].setTotalTwo(totalTwo);
+                break;
+            case 9:
+                masivMinut[8].setTotalOne(totalOne);
+                masivMinut[8].setTotalTwo(totalTwo);
+                break;
+
+        }
+    }
+
+
+    public void setResultTotal(int timess,  int totalOne, int totalTwo){
+        if(timess >= 600 ){
+            resultTotal = totalOne + totalTwo;
+        }
+    }
+
+
+
+
+
+
+
+    public class Minute {
+
+        double predlagaemyiBukmekermTotal;
+        int totalOne;
+        int totalTwo;
+
+
+        public void setPredlagaemyiBukmekermTotal(double predlagaemyiBukmekermTotal) {
+            if (0.0 != predlagaemyiBukmekermTotal) {
+                this.predlagaemyiBukmekermTotal = predlagaemyiBukmekermTotal;
+            }
+        }
+
+        public void setTotalOne(int totalOne) {
+            if (totalOne != 0) {
+                this.totalOne = totalOne;
+            }
+        }
+
+        public void setTotalTwo(int totalTwo) {
+            if (totalTwo != 0) {
+                this.totalTwo = totalTwo;
+            }
+        }
+
+
+        @Override
+        public String toString() {
+            return "Minute{" +
+                    "predlagaemyiBukmekermTotal=" + predlagaemyiBukmekermTotal +
+                    ", totalOne=" + totalOne +
+                    ", totalTwo=" + totalTwo +
+                    '}';
+        }
+    }
+
+
+
+
+
+
+
 }
